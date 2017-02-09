6d512a83ec87ee68cf01ece69358fd12227fb61a
Pol_hp <Polhp@users.noreply.github.com>
2017-02-09 14:17:58 +0100

    Update windows.js

--- windows.js
+++ windows.js
@@ -1,23 +1,21 @@
 const execa = require('execa');
 
-const isOn = () => 
-	execa.shell('ping google.com -n 1 -w 1000').then(() => true).catch(() => false);
+const isOn = () => execa.shell('ping google.com -n 1 -w 1000').then(() => true).catch(() => false);
 
 const device = () => 
 	execa.shell('netsh interface show interface').then((stdout) => {
 		let data = new Array(stdout);
 		data = data[0].stdout.split(' ');
 
-		if(data == undefined)
+		if(data === undefined)
 			throw new Error('Couldn\'t find a Wi-Fi device');
 
 		return data[data.length - 1].trim();
 	});
 
-const toggleDevice = (turnOn) => 
-	device().then(deviceInterface => execa.shell(`netsh interface set interface "${deviceInterface}" ${(turnOn) ? 'enabled' : 'disabled'}`).catch(err => {throw new Error('You don\'t have any WI-FI device or your run this without administrator privilegies!')}));
+const toggleDevice = turnOn => device().then(deviceInterface => execa.shell(`netsh interface set interface "${deviceInterface}" ${(turnOn) ? 'enabled' : 'disabled'}`).catch(() => {throw new Error('You don\'t have any WI-FI device or your run this without administrator privilegies!')}));
 
-const toggle = (turnOn) => {
+const toggle = turnOn => {
 	if (typeof turnOn === 'boolean') {
 		return toggleDevice(turnOn);
 	}
