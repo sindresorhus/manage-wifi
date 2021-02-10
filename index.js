45f8f5298ac294f527168ed86d425f6dba9e53f6
Atakan Atamert <32536245+Naktrem@users.noreply.github.com>
2021-02-11 01:09:47 +0300

    Added Windows support.

--- index.js
+++ index.js
@@ -3,29 +3,67 @@ import pMemoize from 'p-memoize';
 import delay from 'delay';
 
 const getDevice = pMemoize(async () => {
-	if (process.platform !== 'darwin') {
-		throw new Error('macOS only');
+	if (process.platform == 'darwin') {
+		const { stdout } = await execa('networksetup', ['-listallhardwareports']);
+		const result = /Hardware Port: Wi-Fi\nDevice: (?<device>en\d)/.exec(stdout);
+		if (!result) {
+			throw new Error('Couldn\'t find a Wi-Fi device');
+		}
+
+		return result.groups.device;
+	} else if (process.platform == 'win32') {
+		const { stdout } = await execa('powershell', [
+			'-command',
+			'Get-NetAdapter -Name Wi-Fi | Format-List -Property PnPDeviceID',
+		]);
+
+		const result = /(?<=PnPDeviceID : ).*/.exec(stdout);
+
+		if (!result) {
+			throw new Error('Couldn\'t find a Wi-Fi device');
+		}
+
+		return result[0];
+	} else {
+		throw new Error('macOS or Windows only');
 	}
-
-	const {stdout} = await execa('networksetup', ['-listallhardwareports']);
-
-	const result = /Hardware Port: Wi-Fi\nDevice: (?<device>en\d)/.exec(stdout);
-	if (!result) {
-		throw new Error('Couldn\'t find a Wi-Fi device');
-	}
-
-	return result.groups.device;
 });
 
-const isOn = async device => {
-	const {stdout} = await execa('ifconfig', [device]);
-	return stdout.includes('status: active');
+const isOn = async (device) => {
+	if (process.platform == 'darwin') {
+		const { stdout } = await execa('ifconfig', [device]);
+		return stdout.includes('status: active');
+	} else if (process.platform == 'win32') {
+		const { stdout } = await execa('powershell', [
+			'-command',
+			'Get-NetAdapterAdvancedProperty -Name Wi-Fi -RegistryKeyword RFOff -AllProperties | Format-List -Property RegistryValue',
+		]);
+		const status = /(?<=\{).+?(?=\})/.exec(stdout);
+		return !parseInt(status);
+	}
 };
 
 const toggleDevice = async (device, turnOn) => {
-	await execa('networksetup', ['-setairportpower', device, (turnOn ? 'on' : 'off')]);
+	if (process.platform == 'darwin') {
+		await execa('networksetup', [
+			'-setairportpower',
+			device,
+			turnOn ? 'on' : 'off',
+		]);
+		await delay(100);
+	} else if (process.platform == 'win32') {
+		var setStatus = turnOn ? 0 : 1;
+		//console.log("Testing..."); // Odd Issue: Without this log statement, tests fail on Windows 10
 
-	await delay(100);
+		await execa(
+			'powershell -command Start-Process PowerShell -Verb RunAs -WindowStyle Hidden',
+			[
+				`-ArgumentList "Set-NetAdapterAdvancedProperty -Name Wi-Fi -RegistryKeyword RFOff -AllProperties -RegistryValue ${setStatus}"`,
+			]
+		);
+		await delay(6000);
+	}
+	
 	const on = await isOn(device);
 
 	const shouldRetry = turnOn ? !on : on;
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import manageWifi from './index.js';
 
-if (process.env.CI || process.platform !== 'darwin') { // CI doesn't have Wi-Fi
+if (process.env.CI || process.platform !== 'darwin' && process.platform !== 'win32') { // CI doesn't have Wi-Fi
 	test('ci', t => {
 		t.pass();
 	});
@@ -12,7 +12,11 @@ if (process.env.CI || process.platform !== 'darwin') { // CI doesn't have Wi-Fi
 	});
 
 	test('main', async t => {
-		t.regex(await manageWifi.device(), /en\d/);
+		if(process.platform == 'darwin'){
+			t.regex(await manageWifi.device(), /en\d/);
+		}else if(process.platform == 'win32'){
+			t.regex(await manageWifi.device(), /PCI|USB/);
+		}
 		await manageWifi.off();
 		t.false(await manageWifi.isOn());
 		await manageWifi.on();
